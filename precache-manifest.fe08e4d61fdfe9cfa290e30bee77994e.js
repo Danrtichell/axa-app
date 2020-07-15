d8190324f59294c768a42169cf77d8da12dc968c
Dan Guba <HenrikMBP@henriks-mbp.lan>
2020-07-16 01:57:40 +0800

    Updates

---
+++ precache-manifest.fe08e4d61fdfe9cfa290e30bee77994e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b408c91042f17bc82eaa761f28164b04",
+    "url": "/Danrtichell/axa-app.git/index.html"
+  },
+  {
+    "revision": "0b4a7cc341dbdd1b6bd5",
+    "url": "/Danrtichell/axa-app.git/static/css/2.4c4477be.chunk.css"
+  },
+  {
+    "revision": "13894ade45fa3464f134",
+    "url": "/Danrtichell/axa-app.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0b4a7cc341dbdd1b6bd5",
+    "url": "/Danrtichell/axa-app.git/static/js/2.da190201.chunk.js"
+  },
+  {
+    "revision": "f576af3254df0679363d3e6ffe804e36",
+    "url": "/Danrtichell/axa-app.git/static/js/2.da190201.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13894ade45fa3464f134",
+    "url": "/Danrtichell/axa-app.git/static/js/main.cfc05f28.chunk.js"
+  },
+  {
+    "revision": "40bced6a68067d9297bd",
+    "url": "/Danrtichell/axa-app.git/static/js/runtime-main.6156c516.js"
+  }
+]);
